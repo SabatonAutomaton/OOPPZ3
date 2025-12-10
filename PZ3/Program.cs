4b84e4027332e9ed33785dab9a849a04bcfe3682
Артём Рубцов <artyomrubtsow@yandex.ru>
2025-12-10 21:43:08 +0700

    Добавлено расширение для юнит тестов

---
+++ PZ3/Program.cs
@@ -0,0 +1,22 @@
+﻿using static PZ3.Expression;
+
+namespace PZ3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Variable("x");
+            var y = new Variable("y");
+            var c = new Constant(3);
+            Expression expr1 = ((-4) * x) * (3 * x + y * y) / 5;
+            Expression expr2 = (5 - 3 * c) * Sqrt(16 + c * c);
+
+            Console.WriteLine(expr1);
+            Console.WriteLine(expr1.Compute(new Dictionary<string, double> { ["x"] = 1, ["y"] = 2 }));
+            Console.WriteLine(expr2);
+            Console.WriteLine(expr2.Compute(new Dictionary<string, double> { ["x"] = 1, ["y"] = 2 }));
+            Console.ReadLine();
+        }
+    }
+}
